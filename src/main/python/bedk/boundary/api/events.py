c15bcb5b4a933268c935a9180a6174e5452403e8
jdgwartney <davidg@boundary.com>
2014-02-28 15:45:37 -0800

    Example python files to put under control

---
+++ src/main/python/bedk/boundary/api/events.py
@@ -0,0 +1,6 @@
+#
+# events.py
+#
+
+def credentials(org_id,api_key):
+    print(org_id + ':' + api_key)
---
+++ src/main/python/bedk/sendevents.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import boundary.api.events
+
+
+
+org_id = "<Organization ID>"
+api_key = "<API key>"
+
+
+
+boundary.events.api.credentials(org_id,api_key)
