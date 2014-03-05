fa11d77633a9664a657eaaa13706a6389af3e404
jdgwartney <davidg@boundary.com>
2014-03-04 22:14:26 -0800

    First complete set of tests for the Severity enumeration

--- src/main/python/bedk/boundary/api/test/TestSeverity.py
+++ src/main/python/bedk/boundary/api/test/TestSeverity.py
@@ -4,7 +4,7 @@ Created on Mar 4, 2014
 @author: davidg
 '''
 import unittest
-from boundary.api import EventSeverity
+from boundary.api.events import SEVERITY
 
 class TestSeverity(unittest.TestCase):
 
@@ -16,11 +16,31 @@ class TestSeverity(unittest.TestCase):
     def tearDown(self):
         pass
 
-
-    def testEnumInfo(self):
+    def testEnumCount(self):
+        self.assertTrue(len(list(SEVERITY)) == 4,'Check enumeration count')
+    
+    def testEnumMembers(self):
         pass
-#         member = EventSeverity.INFO
-#         self.assertTrue(member.INFO.value == "INFO",'Check to see if INFO is defined')
+        
+    def testEnumEquality(self):
+        self.assertTrue(SEVERITY.CRITICAL == SEVERITY.CRITICAL, 'Check for equality')
+        self.assertTrue(SEVERITY.ERROR == SEVERITY.ERROR, 'Check ERROR for equality')
+        self.assertTrue(SEVERITY.INFO == SEVERITY.INFO, 'Check INFO for equality')
+        self.assertTrue(SEVERITY.WARN == SEVERITY.WARN, 'Check WARN for equality')
+        
+    def testEnumSet(self):
+        self.assertTrue(isinstance(SEVERITY.CRITICAL,SEVERITY))
+        self.assertTrue(isinstance(SEVERITY.ERROR,SEVERITY))
+        self.assertTrue(isinstance(SEVERITY.INFO,SEVERITY))
+        self.assertTrue(isinstance(SEVERITY.WARN,SEVERITY))
+    #
+    # NOTE: PyDev highlights these as syntax errors but it is incorrect
+    #
+    def testEnumValues(self):
+        self.assertEqual(str(SEVERITY.CRITICAL.value),'CRITICAL','Check CRITICAL string value')
+        self.assertEqual(str(SEVERITY.ERROR.value),'ERROR','Check ERROR string value')
+        self.assertEqual(str(SEVERITY.INFO.value),'INFO','Check INFO string value')
+        self.assertEqual(str(SEVERITY.WARN.value),'WARN','Check WARN string value')
 
 
 if __name__ == "__main__":
